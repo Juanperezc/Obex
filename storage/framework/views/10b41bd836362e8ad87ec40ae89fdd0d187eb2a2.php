9a4d3671d244e9dc9c7bc46910585ef2034cade1
Marco Padua <mpadua1995@gmail.com>
2018-03-07 14:12:02 -0400

    cambios home

--- storage/framework/views/10b41bd836362e8ad87ec40ae89fdd0d187eb2a2.php
+++ storage/framework/views/10b41bd836362e8ad87ec40ae89fdd0d187eb2a2.php
@@ -1,14 +1,12 @@
-@extends('layouts.app')
+<?php $__env->startSection('title', 'Main page'); ?>
 
-@section('title', 'Main page')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
     <div class="wrapper wrapper-content animated fadeInRight">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center m-t-lg">
                             <h1>
-                                Welcome in Obex Laravel Starter Project
+                                Welcome in INSPINIA Laravel Starter Project
                             </h1>
                             <small>
                                 It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
@@ -17,4 +15,6 @@
                     </div>
                 </div>
             </div>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
