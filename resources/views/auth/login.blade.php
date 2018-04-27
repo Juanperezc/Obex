ba93de07ce21a170e697fad96c7db2c861b7bf82
Marco Saenz <marco.saenz262@gmail.com>
2018-04-27 00:37:32 -0400

    Cambios al login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,15 +9,13 @@
 
             </div>
             <h3>Bienvenido a Obex</h3>
-            <p>lorem ipsum lorem array_intersect_assoc
-            Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+            <p>
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <p>Login in. To see it in action.</p>
             <form class="m-t" role="form" method="POST" action="{{ route('login') }}">
             {{ csrf_field() }}
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <input type="email" name="email" class="form-control" placeholder="Email" value="{{ old('email') }}" required="">
+                    <input type="email" name="email" class="form-control" placeholder="Correo" value="{{ old('email') }}" required="">
                     @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -25,18 +23,19 @@
                     @endif
                 </div>
                 <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
+                    <input type="password" name="password" class="form-control" placeholder="Contraseña" required="">
                     @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
                 </div>
-                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+                <button type="submit" class="btn btn-primary block full-width m-b">Ingresar</button>
 
-                <a href="#"><small>Olvidaste tu contraseña? pues te jodes</small></a>
+                <a href="#">¿Olvidaste tu contraseña?</a>
+                <br>
+                <a href="#">Contacta con el administrador</a>
             </form>
-            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 © 2014</small> </p>
         </div>
 </div>
 @endsection
