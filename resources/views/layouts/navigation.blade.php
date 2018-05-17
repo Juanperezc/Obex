e242aa77a2b8aa533121a7491daf1e030181d26e
Juan Perez <juanl1996@hotmail.com>
2018-05-16 21:47:50 -0400

    navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -67,81 +67,9 @@
                         </li>
                     </ul>
                 </li>
-                @endrole @role('admin|manager')
-                <li
-                    class="{{ areActiveRoutes(['project.view','project.create','project.resources','project.modify']) }}">
-                    <a href="#">
-                        <i class="fa fa-book"></i>
-                        <span class="nav-label">Proyectos</span>
-                    </a>
-                    <ul class="nav nav-second-level collapse">
-                        <li class="{{ isActiveRoute('project.view') }}">
-                            <a href="{{ url('/project/view') }}">
-                                <i class="fa fa-eye"></i>Ver Proyectos</a>
-                        </li>
-                        <li class="{{ isActiveRoute('project.create') }}">
-                            <a href="{{ url('/project/create') }}">
-                                <i class="fa fa-plus"></i>Crear Proyecto</a>
-                        </li>
-                        <li class="{{ isActiveRoute('project.resources') }}">
-                            <a href="{{ url('/project/resources') }}">
-                                <i class="fa fa-stack-overflow"></i>Gestionar Recursos</a>
-                        </li>
-                        <li class="{{ isActiveRoute('project.modify') }}">
-                            <a href="{{ url('/project/modify') }}">
-                                <i class="fa fa-exclamation-triangle"></i>Modificar o eliminar</a>
-                        </li>
-                    </ul>
-                </li>
-                @endrole @role('admin|manager')
-                <li class="{{ areActiveRoutes(['team.view','team.create','team.modify']) }}">
-                    <a href="#">
-                        <i class="fa fa-group"></i>
-                        <span class="nav-label">Equipos</span>
-                    </a>
-                    <ul class="nav nav-second-level collapse">
-                        <li class="{{ isActiveRoute('team.view') }}">
-                            <a href="{{ url('/team/view') }}">
-                                <i class="fa fa-eye"></i>Ver Equipos</a>
-                        </li>
-                        <li class="{{ isActiveRoute('team.create') }}">
-                            <a href="{{ url('/team/create') }}">
-                                <i class="fa fa-plus"></i>Crear Equipo</a>
-                        </li>
-                        <li class="{{ isActiveRoute('team.modify') }}">
-                            <a href="{{ url('/team/modify') }}">
-                                <i class="fa fa-exclamation-triangle"></i>Modificar o eliminar Equipo</a>
-                        </li>
-                    </ul>
-                </li>
-                @endrole @role('admin|manager')
-                <li class="{{ areActiveRoutes(['report.team', 'report.project']) }}">
-                    <a href="#">
-                        <i class="fa fa-pie-chart"></i>
-                        <span class="nav-label">Reportes</span>
-                    </a>
-                    <ul class="nav nav-second-level collapse">
-                        <li class="{{ isActiveRoute('report.team') }}">
-                            <a href="{{ url('/report/team') }}">
-                                <i class="fa fa-group"></i>Equipo</a>
-                        </li>
-                        <li class="{{ isActiveRoute('report.project') }}">
-                            <a href="{{ url('/report/project') }}">
-                                <i class="fa fa-stack-overflow"></i>Proyecto</a>
-                        </li>
-                    </ul>
-                </li>
                 @endrole
+         
 
-                <li class="{{ isActiveRoute('manage-notification') }}">
-                    <a href="{{ url('/manage-notification') }}">
-                        <i class="fa fa-bullhorn"></i>
-                        <span class="nav-label">Notificaciones al cliente(?)</span>
-                    </a>
-                </li>
-            </ul>
-            </li>
-            @endrole
             @role('admin|manager')
             <li class="{{ areActiveRoutes(['project.view','project.create','project.resources','project.modify']) }}">
                 <a href="#"><i class="fa fa-book"></i> <span class="nav-label">Proyectos</span> </a>
