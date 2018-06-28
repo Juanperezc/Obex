8c4cde4401d5b20768e9219608e289c4ed3c6a67
Juan Perez <juanl1996@hotmail.com>
2018-06-28 02:29:53 -0400

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,6 @@
 <div class="footer">
     <div class="pull-right">
-<<<<<<< HEAD
         {{ getActualdate() }}
-=======
->>>>>>> 2eaf69fe3bf0458ab4165027f791ea84de9619bd
     </div>
     <div>
         <strong>Copyright</strong> Obex &copy; 2018
